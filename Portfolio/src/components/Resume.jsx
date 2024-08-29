e6044cecbb5e00461f4f7d4d55dd6f2a8ad8b8ce
Xavier <xavier3435@icloud.com>
2024-08-29 13:44:27 -0400

    contact & resume

---
+++ Portfolio/src/components/Resume.jsx
@@ -0,0 +1,22 @@
+// src/components/Resume.jsx
+function Resume() {
+    return (
+      <section>
+        <h2>Resume</h2>
+        <a href="/path/to/your/resume.pdf" download>Download My Resume</a>
+        <h3>Proficiencies</h3>
+        <ul>
+          <li>JavaScript (ES6+)</li>
+          <li>React</li>
+          <li>HTML/CSS</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MongoDB</li>
+          {/* Add more proficiencies as needed */}
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Resume;
+  
