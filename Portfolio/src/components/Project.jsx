7dd489181b7421369398dea0d169a336faa72027
Xavier <xavier3435@icloud.com>
2024-09-18 13:33:57 -0400

    added proj image

--- Portfolio/src/components/Project.jsx
+++ Portfolio/src/components/Project.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 function Project({ title, image, liveLink, repoLink }) {
   return (
     <div className="project">
-      <img src="/images/proj1.png" alt={title} className="project-image" />  {/* Use an absolute path */}
+      <img src="/images/Screenshot 2024-09-18 132630.png" alt={Nomad} className="project-image"  />  
       <h3 className="project-title">{title}</h3>
-      <a href={liveLink} target="_blank" rel="noopener noreferrer">Live Site</a>
-      <a href={repoLink} target="_blank" rel="https://github.com/lateralmonster1/social-network-api.git">GitHub Repo</a>
+      <a href={liveLink} target="_blank" rel="https://breannacamacho.github.io/Nomad/">Live Site</a>
+      <a href={repoLink} target="_blank" rel="https://github.com/lateralmonster1/Nomad.git">GitHub Repo</a>
     </div>
   );
 }
