d1ad1bdbd48ac868208cfb0ae0b1933a078e574f
Xavier <xavier3435@icloud.com>
2024-09-18 13:08:20 -0400

    added my photo

--- Portfolio/src/components/AboutMe.jsx
+++ Portfolio/src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ function AboutMe() {
     return (
       <section>
         <h2>About Me</h2>
-        <img src="/path/to/your/photo.jpg" alt="Xavier Leon" />
+        <img src="../images/professional-pic.JPEG" alt="Xavier Leon" />
         <p>Hello! I'm Xavier Leon, a passionate web developer with experience in building dynamic web applications. </p>
       </section>
     );
